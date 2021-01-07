9812fc4ae495fa57d86098e217d9eb7f9a5abec4
Haibara-4869 <963545385@qq.com>
2021-01-07 14:57:42 +0800

    \345\210\206\346\225\260\347\233\270\345\212\240.c

---
+++ 分解质因数.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	scanf("%d",&n);
+for(i=2;i<=n; i++){
+		while(n!=i){
+			if(n%i==0){
+				printf("%d*",i);
+				n=n/i;
+			}else
+				break;
+		}
+	}
+	printf("%d\n",n);
+}
+
+ 
---
+++ 判断位数.c
@@ -0,0 +1,15 @@
+/* Calculates the number of digits in an integer */
+#include <stdio.h>
+int main(void)
+{
+  int digits = 0, n;
+  printf("Enter a nonnegative integer: ");
+  scanf("%d", &n);
+  while (n > 0) {
+    n /= 10;
+    digits++;
+  } 
+  printf("The number has %d digit(s).\n", digits);
+  return 0;
+}
+
