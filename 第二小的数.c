081031b607b5c94dbbb63bad1eef2a6eda6e6d06
Haibara-4869 <963545385@qq.com>
2021-01-10 20:41:59 +0800

    \347\254\254\344\272\214\345\260\217\347\232\204\346\225\260.c

---
+++ 第二小的数.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,m,i,j,a[99];
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%d",&m);
+		for(j=0;j<m;j++){
+			scanf("%d",&a[j])
+		}
+		for(j=0;j<m;j++){
+			if(a[j]==a[j+1])
+		}
+	}
+}
